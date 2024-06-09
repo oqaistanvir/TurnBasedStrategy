3c4de55bb954be26c0fc87ec0ebec11be527426c
Oqais Tanvir <oqaistanvir@gmail.com>
2024-06-09 21:00:10 +0530

    Crate Destruction

--- Assets/Scripts/DestructibleCrate.cs
+++ Assets/Scripts/DestructibleCrate.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class DestructibleCrate : MonoBehaviour
 {
     public static event EventHandler OnAnyDestroyed;
+    [SerializeField] private Transform crateDestroyedPrefab;
     private GridPosition gridPosition;
     private void Start()
     {
@@ -18,8 +19,25 @@ public class DestructibleCrate : MonoBehaviour
     }
     public void Damage()
     {
+        Transform crateDestroyedTransform = Instantiate(crateDestroyedPrefab, transform.position, transform.rotation);
+        float explosionForce = 150f;
+        float explosionRange = 10f;
+        ApplyExplosionToChildren(crateDestroyedTransform, explosionForce, transform.position, explosionRange);
         Destroy(gameObject);
 
         OnAnyDestroyed?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void ApplyExplosionToChildren(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange)
+    {
+        foreach (Transform child in root)
+        {
+            if (child.TryGetComponent(out Rigidbody childRigidbody))
+            {
+                childRigidbody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+            }
+
+            ApplyExplosionToChildren(child, explosionForce, explosionPosition, explosionRange);
+        }
     }
 }
